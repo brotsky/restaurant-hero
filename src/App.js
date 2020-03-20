478067bc6e963fa9fb563aa8599524ad2ca8db5d
Brandon Brotsky <Brandon@brotskydesigns.com>
2020-03-20 09:33:09 -0700

    submit form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import InstagramEmbed from 'react-instagram-embed';
 import useFetch from 'fetch-suspense';
 import { shuffle } from 'lodash';
+import { ReactTypeformEmbed } from 'react-typeform-embed';
+import { isMobile } from 'react-device-detect';
+
 
 // import logo from './logo.svg';
 import './App.css';
@@ -29,29 +32,36 @@ const MyFetchingComponent = () => {
   const response = useFetch(googleSheet, { method: 'GET' });
   const posts = tsvJSON(response);
   
-  return posts.map((post, index) => post.Instagram !== '' && (
-    <div key={`post-${index}`} className="App-header">
-      <div>
-        <h3>{post.Restaurant}</h3>
-        <h5><a href={`https://maps.google.com/?q=${post.Location}`} target="_blank" rel="noopener noreferrer">{post.Location}</a></h5>
-        <h5><a href={`tel:${post.Phone.replace(/\D/g,'')}`}>{post.Phone}</a></h5>
-      </div>
-      <InstagramEmbed
-        maxWidth={320}
-        url={post.Instagram}
-        hideCaption={true}
-        containerTagName='div'
-        protocol=''
-        injectScript
-        onLoading={() => {}}
-        onSuccess={() => {}}
-        onAfterRender={() => {}}
-        onFailure={() => {}}
-      />
-    </div>))
+  return posts.map((post, index) => (
+  <>
+    { post.Instagram}
+    { post.Instagram !== '' && (
+      <div key={`post-${index}`} className="App-header">
+        <div>
+          <h3>{post.Restaurant}</h3>
+          <h5><a href={`https://maps.google.com/?q=${post.Location}`} target="_blank" rel="noopener noreferrer">{post.Location}</a></h5>
+          <h5><a href={`tel:${post.Phone.replace(/\D/g,'')}`}>{post.Phone}</a></h5>
+        </div>
+        <InstagramEmbed
+          maxWidth={320}
+          url={post.Instagram}
+          hideCaption={true}
+          containerTagName='div'
+          protocol=''
+          injectScript
+          onLoading={() => {}}
+          onSuccess={() => {}}
+          onAfterRender={() => {}}
+          onFailure={() => {}}
+        />
+      </div>)
+      }
+    </>
+  ));
 };
 
 function App() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
@@ -59,7 +69,8 @@ function App() {
         <p>Our mission is to keep an updated list of restaurants still serving take out / to go during this crazy time. Please support them by ordering their food.</p>
         <Suspense fallback="Loading...">
           <MyFetchingComponent />
-        </Suspense>
+        </Suspense>{ isMobile ? <a href={'https://greg960960.typeform.com/to/HhOIov'}><button id="submit">Submit a Restaurant</button></a>
+              : <button id="submit" onClick={() => setModalOpen(true)}>Submit a Restaurant</button> }
   <p>If you code and would like to contribute to this project, we welcome you to submit a PR on <a href="https://github.com/brotsky/restaurant-hero" target="_blank" rel="noopener noreferrer">GitHub</a>.<br />We encourage you to fork it and create your own RestaurantHero[major city].com.</p>
         <p>
           {'Website created with ❤️ by '}
@@ -68,6 +79,12 @@ function App() {
           <a href="https://twitter.com/DownToBrotsky" target="_blank" rel="noopener noreferrer">Brandon Brotsky</a>
         </p>
       </header>
+      { modalOpen && <div className={'modal'} onClick={() => setModalOpen(false)}>
+        <div className={'closeButton'} onClick={() => setModalOpen(false)}>X</div>
+        <div className={'modalInner'}>
+          <ReactTypeformEmbed url="https://greg960960.typeform.com/to/HhOIov" />
+        </div>
+      </div> }
     </div>
   );
 }
