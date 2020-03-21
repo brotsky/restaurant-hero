fbe3bd10b507a2ff56c6ec6e81326d7a4f30d9f8
Brandon Brotsky <Brandon@brotskydesigns.com>
2020-03-21 15:20:57 -0700

    Seattle Google Sheet

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const { host } = window.location;
 const googleSheetLA = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR8sQyzK0GFOY3r6p_QQ-b6uprsMPN8uN9piRFPemLoJHI-JBshyzL4YtNIVjGem09ts-q3L55wu79E/pub?gid=0&single=true&output=tsv';
 const googleSheetHouston = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT3a2jeKTk--9jI0Zqdq9h7DbR_lF2Iu5AZwG3ZiPbejaRmJ_0uTiw-6ojM4AVoeBrQJIJwiOgBhG17/pub?gid=0&single=true&output=tsv';
 const googleSheetNYC = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vReDHUmGFgLUmBw0mja1CB6mutjMvVXQEojFgloRbdVnX6s_FxfS78dswO6lAVXRGgM3vsXKhtULkU0/pub?gid=0&single=true&output=tsv';
+const googleSheetSeattle = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTffGfOkLRD87cNhflsYmUDXVK02AKiyxrRJVyLGXzX7652b-qd9w3Gjv9JDW9f6RapKewnigPo0Qrp/pub?gid=0&single=true&output=tsv';
 
 const logoLA = '/restaurant-hero-logo.svg';
 const logoHouston = '/restaurant-hero-logo-houston.svg';
@@ -27,6 +28,8 @@ if (host === 'restaurantherohtx.com' || host === 'www.restaurantherohtx.com') {
   logo = logoHouston;
 } else if (host === 'nyc.restauranthero.org') {
   googleSheet = googleSheetNYC;
+} else if (host === 'seattle.restauranthero.org') {
+  googleSheet = googleSheetSeattle;
 }
 
 // add proxy to avoid CORS issuse
