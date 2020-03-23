53e1100511f5d4f7471a4861fc1d8aee92a2a8da
Brett Reinhard <brett@reinhards.us>
2020-03-23 11:52:20 -0700

    Updated test to ensure Los Angeles data is being loaded by default

--- src/Home.test.js
+++ src/Home.test.js
@@ -1,10 +1,11 @@
 import React from 'react';	
-import { render } from '@testing-library/react';	
+import { render, queryAllByAltText } from '@testing-library/react';	
 import App from './Home';	
 
-test('renders learn react link', () => {	
-  const { getByText } = render(<App />);	
+test('renders Log Angeles Restaurant Hero and contains Submit Button', () => {	
+  const { getByText, container } = render(<App />);	
   const linkElement = getByText(/Submit a Restaurant/i);
-
+  const img = queryAllByAltText(container,"Restaurant Hero Logo");
+  expect(img[0].getAttribute("src")).toBe("/restaurant-hero-logo.svg")
   expect(linkElement).toBeInTheDocument();
 });
